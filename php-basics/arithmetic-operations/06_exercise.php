f2a7152d8495446f659f9ba0de5db620660458e1
Valik <valikfad@gmail.com>
2021-08-14 02:32:10 +0300

    update php-basics/arithmetic-operations/06_exercise.php

--- php-basics/arithmetic-operations/06_exercise.php
+++ php-basics/arithmetic-operations/06_exercise.php
@@ -3,7 +3,7 @@
 for ($i = 1; $i <= 110; $i++)
 {
     $output = "";
-    if ($i % 3 === 0) $output = "Coza";
+    if ($i % 3 === 0) $output .= "Coza";
     if ($i % 5 === 0) $output .= "Loza";
     if ($i % 7 === 0) $output .= "Woza";
 
