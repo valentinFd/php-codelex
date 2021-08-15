ece9e32aec85706391cb573ff082a39ea19e7948
Valik <valikfad@gmail.com>
2021-08-15 15:59:44 +0300

    add classes-and-objects

---
+++ php-basics/classes-and-objects/energy-drinks.php
@@ -0,0 +1,22 @@
+<?php
+
+$surveyed = 12467;
+$purchased_energy_drinks = 0.14;
+$prefer_citrus_drinks = 0.64;
+
+function calculate_energy_drinkers(int $numberSurveyed)
+{
+    throw new LogicException(";(");
+}
+
+function calculate_prefer_citrus(int $numberSurveyed)
+{
+    throw new LogicException(";(");
+}
+
+/*
+fixme
+echo "Total number of people surveyed " . $surveyed;
+echo "Approximately " . ? . " bought at least one energy drink";
+echo ? . " of those " . "prefer citrus flavored energy drinks.";
+*/
