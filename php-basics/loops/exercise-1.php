05c797b5b29bdf8b5d39722f2a0c1822d207d79f
Valik <valikfad@gmail.com>
2021-08-17 13:58:49 +0300

    add loops

---
+++ php-basics/loops/exercise-1.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "The first 10 natural numbers are:";
+
+//todo write a program that displays first 10 natural numbers
---
+++ php-basics/loops/exercise-2.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "Input number of terms: ";
+
+//todo complete loop to multiply i with itself n times, it is NOT allowed to use built-in pow() function
