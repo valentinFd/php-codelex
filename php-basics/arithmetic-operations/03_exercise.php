3e522fca74310b5be3ae4f35d21a377cf5064290
Valik <valikfad@gmail.com>
2021-08-17 12:30:47 +0300

    update 03_exercise.php

--- php-basics/arithmetic-operations/03_exercise.php
+++ php-basics/arithmetic-operations/03_exercise.php
@@ -8,7 +8,7 @@ for ($i = $a; $i <= $b; $i++)
 {
     $sum += $i;
 }
-$average = $sum / $b;
+$average = $sum / ($b - $a + 1);
 
 echo("The sum of $a to $b is $sum" . PHP_EOL);
 echo("The average is $average" . PHP_EOL);
