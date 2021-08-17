66110493d8cd6df562db33150b024b52b66bb0a1
Valik <valikfad@gmail.com>
2021-08-17 15:38:31 +0300

    update 05_exercise.php

--- php-basics/flow-of-control/05_exercise.php
+++ php-basics/flow-of-control/05_exercise.php
@@ -1,5 +1,16 @@
 <?php
 
+$keypad = [
+    "abc" => 2,
+    "def" => 3,
+    "ghi" => 4,
+    "jkl" => 5,
+    "mno" => 6,
+    "pqrs" => 7,
+    "tuv" => 8,
+    "wxyz" => 9
+];
+
 do
 {
     $string = strtolower((string)readline("Enter a string: "));
@@ -9,50 +20,13 @@ $result = "";
 $stringArray = str_split($string);
 foreach ($stringArray as $char)
 {
-    switch ($char)
+    foreach ($keypad as $key => $value)
     {
-        case "a":
-        case "b":
-        case "c":
-            $result .= "2";
-            break;
-        case "d":
-        case "e":
-        case "f":
-            $result .= "3";
-            break;
-        case "g":
-        case "h":
-        case "i":
-            $result .= "4";
-            break;
-        case "j":
-        case "k":
-        case "l":
-            $result .= "5";
-            break;
-        case "m":
-        case "n":
-        case "o":
-            $result .= "6";
-            break;
-        case "p":
-        case "q":
-        case "r":
-        case "s":
-            $result .= "7";
-            break;
-        case "t":
-        case "u":
-        case "v":
-            $result .= "8";
-            break;
-        case "w":
-        case "x":
-        case "y":
-        case "z":
-            $result .= "9";
-            break;
+        $pos = strpos($key, $char);
+        if ($pos !== false)
+        {
+            $result .= str_repeat($value, $pos + 1);
+        }
     }
 }
 echo $result . PHP_EOL;
