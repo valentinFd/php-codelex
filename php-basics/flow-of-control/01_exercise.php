ab9aea3dd137e3ae1af3dde77bb6946eb455337e
Valik <valikfad@gmail.com>
2021-08-17 11:50:28 +0300

    01 exercise done

--- php-basics/flow-of-control/01_exercise.php
+++ php-basics/flow-of-control/01_exercise.php
@@ -1,1 +1,7 @@
 <?php
+
+$a = (int)readline("Input the 1st number: ");
+$b = (int)readline("Input the 2nd number: ");
+$c = (int)readline("Input the 3rd number: ");
+
+echo max(max($a, $b), $c) . PHP_EOL;
