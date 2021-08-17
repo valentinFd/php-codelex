636b109a5742ed9a34db487beb0b3692ab6af6e6
Valik <valikfad@gmail.com>
2021-08-17 14:06:17 +0300

    04 exercise done

---
+++ php-basics/loops/04_exercise.php
@@ -0,0 +1,14 @@
+<?php
+
+$number = (int)readline("Enter a number: ");
+
+for ($i = 1; $i <= $number; $i++)
+{
+    $result = "";
+    if ($i % 3 === 0) $result .= "Fizz";
+    if ($i % 5 === 0) $result .= "Buzz";
+    if (!($i % 3 === 0 || $i % 5 === 0))$result = $i;
+
+    if ($i % 20 === 0) echo $result . PHP_EOL;
+    else echo "$result ";
+}
