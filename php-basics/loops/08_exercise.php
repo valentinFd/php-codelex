3aa5ed1ba32672ee5daac9029bcc1f9c01409717
Valik <valikfad@gmail.com>
2021-08-23 18:42:03 +0300

    08 exercise done

---
+++ php-basics/loops/08_exercise.php
@@ -0,0 +1,30 @@
+<?php
+
+class NumberSquare
+{
+    private int $min;
+
+    private int $max;
+
+    public function start()
+    {
+        $this->min = (int)readline("Min: ");
+        $this->max = (int)readline("Max: ");
+        for ($i = $this->min; $i <= $this->max; $i++)
+        {
+            $row = "";
+            for ($j = $i; $j <= $this->max; $j++)
+            {
+                $row .= $j;
+            }
+            for ($k = 1; $k < $i; $k++)
+            {
+                $row .= $k;
+            }
+            echo $row . PHP_EOL;
+        }
+    }
+}
+
+$program = new NumberSquare;
+$program->start();
