4fd215bc84fb4927ec9f1cc282550981fa4264b9
Valik <valikfad@gmail.com>
2021-08-26 20:38:32 +0300

    make possibility to set number of rows and columns

--- exercises/slot_machine.php
+++ exercises/slot_machine.php
@@ -2,7 +2,32 @@
 
 class SlotMachine
 {
-    private const SIZE = 3;
+    // number of columns >= number of rows.
+    // number of rows and columns can be changed.
+    private const ROWS = 3;
+
+    private const COLS = 4;
+
+    // slot machine's elements that get randomly chosen.
+    private const ELEMENTS = ["A", "B", "C", "D", "E"];
+
+    // used to determine whether a line consists of the same elements. Each value is a prime number.
+    private const ELEMENT_SCORES = [
+        "A" => 2,
+        "B" => 3,
+        "C" => 5,
+        "D" => 7,
+        "E" => 11
+    ];
+
+    // winning amount for one line consisting of the same element.
+    private const ELEMENT_WIN_AMOUNTS = [
+        "A" => 5,
+        "B" => 10,
+        "C" => 15,
+        "D" => 25,
+        "E" => 50
+    ];
 
     private const WINNING_MULTIPLIERS = [
         10 => 1,
@@ -12,66 +37,88 @@ class SlotMachine
         160 => 5
     ];
 
-    private const ELEMENTS = ["A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "B", "B", "B", "B", "C", "C", "C", "C", "D", "D", "E"];
-
-    private const ELEMENT_SCORES = [
-        "A" => 2,
-        "B" => 3,
-        "C" => 5,
-        "D" => 7,
-        "E" => 11
-    ];
-
-    private const ELEMENT_WIN_AMOUNTS = [
-        "A" => 5,
-        "B" => 10,
-        "C" => 15,
-        "D" => 25,
-        "E" => 300
-    ];
-
     private int $winnings;
 
     private array $slotMachine;
 
+    // used to determine whether a line consists of the same element.
     private array $scores;
 
     public function __construct()
     {
         $this->winnings = 0;
         $this->slotMachine = [];
-        for ($i = 0; $i < self::SIZE; $i++)
-        {
-            $slotMachine[] = array_fill(0, self::SIZE + 1, "");
-        }
-        $this->scores = array_fill(0, 2 * self::SIZE + 2, 1);
+        // creating an array that stores scores of all rows, columns and diagonals.
+        // size of array is sum of: number of rows, number of columns and number of diagonals.
+        // number of rows is ROWS, number of columns is COLS, number of diagonals is 2 + 2 * (COLS - ROWS).
+        $this->scores = array_fill(0, self::ROWS + self::COLS + 2 + 2 * (self::COLS - self::ROWS), 1);
     }
 
     private function startSlotMachine(int $amount)
     {
-        for ($i = 0; $i < self::SIZE; $i++)
+        for ($i = 0; $i < self::ROWS; $i++)
         {
-            for ($j = 0; $j < self::SIZE; $j++)
+            for ($j = 0; $j < self::COLS; $j++)
             {
                 $randomElement = self::ELEMENTS[array_rand(self::ELEMENTS)];
-                $this->slotMachine[$i][$j] = $randomElement;
+                $this->slotMachine[$i][] = $randomElement;
 
-                $this->scores[$i] *= self::ELEMENT_SCORES[$randomElement];
-                $this->scores[self::SIZE + $j] *= self::ELEMENT_SCORES[$randomElement];
-                if ($i === $j)
+                $this->scores[$i] *= self::ELEMENT_SCORES[$randomElement]; // i row's score.
+                $this->scores[self::ROWS + $j] *= self::ELEMENT_SCORES[$randomElement]; // j column's score.
+                for ($k = 0; $k <= (self::COLS - self::ROWS); $k++)
                 {
-                    $this->scores[2 * self::SIZE] *= self::ELEMENT_SCORES[$randomElement];
-                }
-                if ($i === self::SIZE - 1 - $j)
-                {
-                    $this->scores[2 * self::SIZE + 1] *= self::ELEMENT_SCORES[$randomElement];
+                    // scores of all diagonals that go from top left to bottom right.
+                    if ($j - $i === $k || ($i === 0 && $j < $k) || ($i === self::ROWS - 1 && $j > self::ROWS - 1 + $k))
+                    {
+                        $this->scores[self::ROWS + self::COLS + $k] *= self::ELEMENT_SCORES[$randomElement];
+                    }
+                    // scores of all diagonals that go from top right to bottom left.
+                    if ($i === self::COLS - 1 - $j - $k
+                        || ($i === 0 && $j > self::COLS - 1 - $k)
+                        || ($i === self::ROWS - 1 && $j < self::COLS - self::ROWS - $k))
+                    {
+                        $this->scores[self::ROWS + self::COLS + self::COLS - self::ROWS + 1 + $k] *= self::ELEMENT_SCORES[$randomElement];
+                    }
                 }
             }
         }
-        foreach ($this->scores as $score)
+        var_dump($this->scores);
+        for ($i = 0; $i < self::ROWS; $i++) // check each row's score.
         {
-            $value = pow($score, 1 / self::SIZE);
-            // if $score is a perfect cube, one line of matrix has one kind of element
+            // if $score is a perfect power of COLS (number of columns), $i row consists of the same element.
+            // number of columns is equal to number of elements in one row.
+            // element is determined by calculating pow($score, 1 / COLS), and checking the resulting value's key
+            // in ELEMENT_SCORES.
+            $score = $this->scores[$i];
+            $value = pow($score, 1 / self::COLS);
+            if ((string)$value === (string)round($value))
+            {
+                $value = (int)(string)$value;
+                $this->winnings += (self::ELEMENT_WIN_AMOUNTS[array_search($value, self::ELEMENT_SCORES)]);
+            }
+        }
+        for ($i = self::ROWS; $i < self::ROWS + self::COLS; $i++) // check each column's score.
+        {
+            // if $score is a perfect power of ROWS (number of rows), ($i - ROWS) column consists of the same element.
+            // number of rows is equal to number of elements in one column.
+            // element is determined by calculating pow($score, 1 / ROWS), and checking the resulting value's key
+            // in ELEMENT_SCORES.
+            $score = $this->scores[$i];
+            $value = pow($score, 1 / self::ROWS);
+            if ((string)$value === (string)round($value))
+            {
+                $value = (int)(string)$value;
+                $this->winnings += (self::ELEMENT_WIN_AMOUNTS[array_search($value, self::ELEMENT_SCORES)]);
+            }
+        }
+        for ($i = self::ROWS + self::COLS; $i < self::ROWS + self::COLS + 2 + 2 * (self::COLS - self::ROWS); $i++)
+        {
+            // if $score is a perfect power of COLS (number of columns), $i diagonal consists of the same element.
+            // number of columns is equal to number of elements in one diagonal.
+            // element is determined by calculating pow($score, 1 / COLUMNS), and checking the resulting value's key
+            // in ELEMENT_SCORES.
+            $score = $this->scores[$i];
+            $value = pow($score, 1 / self::COLS);
             if ((string)$value === (string)round($value))
             {
                 $value = (int)(string)$value;
@@ -83,20 +130,20 @@ class SlotMachine
 
     private function displaySlotMachine()
     {
-        for ($i = 0; $i < self::SIZE; $i++)
+        for ($i = 0; $i < self::COLS; $i++)
         {
             echo "--";
         }
         echo PHP_EOL;
-        for ($i = 0; $i < self::SIZE; $i++)
+        for ($i = 0; $i < self::ROWS; $i++)
         {
-            for ($j = 0; $j < self::SIZE; $j++)
+            for ($j = 0; $j < self::COLS; $j++)
             {
                 echo "{$this->slotMachine[$i][$j]} ";
             }
             echo PHP_EOL;
         }
-        for ($i = 0; $i < self::SIZE; $i++)
+        for ($i = 0; $i < self::COLS; $i++)
         {
             echo "--";
         }
@@ -120,16 +167,16 @@ class SlotMachine
         echo "-------------" . PHP_EOL;
     }
 
-    public function getMultiplierCosts(): array
-    {
-        return array_keys(self::WINNING_MULTIPLIERS);
-    }
-
     public function startGame(int $amount): int
     {
         $this->startSlotMachine($amount);
         $this->displaySlotMachine();
         return $this->getWinnings();
+    }
+
+    public function isValidBet(int $bet): bool
+    {
+        return in_array($bet, array_keys(self::WINNING_MULTIPLIERS));
     }
 }
 
@@ -163,7 +210,7 @@ class Person
             {
                 $bet = (int)readline("Enter your bet (0 to quit): $");
                 if ($bet === 0) return;
-                if (!$this->isValidBet($bet, $game->getMultiplierCosts()))
+                if (!$game->isValidBet($bet))
                 {
                     echo "Invalid bet" . PHP_EOL;
                 }
@@ -171,23 +218,15 @@ class Person
                 {
                     echo "Not enough money" . PHP_EOL;
                 }
-            } while (!($this->isValidBet($bet, $game->getMultiplierCosts()) && $this->hasEnoughMoney($bet)));
-            if ($this->hasEnoughMoney($bet))
-            {
-                $this->withdrawCash($bet);
-                $this->depositCash($game->startGame($bet));
-            }
+            } while (!($game->isValidBet($bet) && $this->hasEnoughMoney($bet)));
+            $this->withdrawCash($bet);
+            $this->depositCash($game->startGame($bet)); // start the game and deposit the returned amount of money
         } while (true);
     }
 
-    private function isValidBet(int $bet, array $multiplierCosts): bool
+    private function hasEnoughMoney(int $amount): bool
     {
-        return in_array($bet, $multiplierCosts);
-    }
-
-    private function hasEnoughMoney(int $bet): bool
-    {
-        return $this->cash >= $bet;
+        return $this->cash >= $amount;
     }
 }
 
