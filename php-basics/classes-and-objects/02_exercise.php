5e64891afaf21afca65a4459109821a6c04e32ce
Valik <valikfad@gmail.com>
2021-09-06 12:11:53 +0300

    update 02_exercise.php

--- php-basics/classes-and-objects/02_exercise.php
+++ php-basics/classes-and-objects/02_exercise.php
@@ -11,7 +11,7 @@ class Point
         $this->y = $y;
     }
 
-    public function __toString()
+    public function __toString(): string
     {
         return "($this->x, $this->y)" . PHP_EOL;
     }
