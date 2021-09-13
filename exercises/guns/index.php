16be528428cd841cb8e35b75038ece498fd5ea50
Valik <valikfad@gmail.com>
2021-09-13 17:14:54 +0300

    update index.php

--- exercises/guns/index.php
+++ exercises/guns/index.php
@@ -8,8 +8,8 @@ require_once("SniperRifle.php");
 
 $shop = new GunShop();
 
-$shotgun = new Shotgun("M58B", "A");
-$sniper = new SniperRifle("SW-04J", "B");
+$shotgun = new Shotgun("M58B");
+$sniper = new SniperRifle("SW-04J");
 
 $shop->addGun($shotgun);
 $shop->addGun($sniper);
