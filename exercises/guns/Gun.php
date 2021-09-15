99beeecf0d63792494273a76b8430068dbe3208b
Valik <valikfad@gmail.com>
2021-09-15 14:30:36 +0300

    update Gun.php

--- exercises/guns/Gun.php
+++ exercises/guns/Gun.php
@@ -1,20 +1,30 @@
 <?php
 declare(strict_types = 1);
 
-class Gun
+abstract class Gun
 {
     protected string $name;
 
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    protected int $price;
+
+    public function getPrice(): int
+    {
+        return $this->price;
+    }
+
     protected array $licenses;
 
-    protected function __construct(string $name)
+    protected function __construct(string $name, int $price)
     {
         $this->name = $name;
+        $this->price = $price;
         $this->licenses[] = "Z";
     }
 
-    protected function getTrajectory(): int
-    {
-        return 0;
-    }
+    abstract function getTrajectory(): int;
 }
