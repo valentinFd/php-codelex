4e9cd69e644d9ba7eaee1effa5860f1381e22697
Valik <valikfad@gmail.com>
2021-09-15 14:53:59 +0300

    update PaymentMethod.php

--- exercises/guns/PaymentMethod.php
+++ exercises/guns/PaymentMethod.php
@@ -3,5 +3,17 @@ declare(strict_types = 1);
 
 abstract class PaymentMethod
 {
+    protected int $money;
+
+    public function getMoney(): int
+    {
+        return $this->money;
+    }
+
+    public function __construct(int $money)
+    {
+        $this->money = $money;
+    }
+
     abstract function withdraw(int $amount): void;
 }
