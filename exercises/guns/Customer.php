a883c3c9e1bcf63c7dee736582f465e0839669ca
Valik <valikfad@gmail.com>
2021-09-15 15:16:39 +0300

    update Customer.php

--- exercises/guns/Customer.php
+++ exercises/guns/Customer.php
@@ -31,11 +31,11 @@ class Customer
         $this->bank = new Bank($bankMoney);
     }
 
-    public function printMoney(): string
+    public function printMoney(): void
     {
         $walletDollars = round(($this->wallet->getMoney() / 100), 2);
         $payPalDollars = round(($this->payPal->getMoney() / 100), 2);
         $bankDollars = round(($this->bank->getMoney() / 100), 2);
-        return "Wallet: $$walletDollars | PayPal: $$payPalDollars | Bank: $$bankDollars";
+        echo "Wallet: $$walletDollars | PayPal: $$payPalDollars | Bank: $$bankDollars";
     }
 }
