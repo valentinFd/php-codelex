dd4e66bb5d6b342ccf0563fd88c11ef1b7d92be0
Valik <valikfad@gmail.com>
2021-09-15 14:31:23 +0300

    add various payment methods

--- exercises/guns/GunShop.php
+++ exercises/guns/GunShop.php
@@ -10,12 +10,40 @@ class GunShop
         $this->guns = [];
     }
 
-    public function addGun(Gun $gun)
+    public function addGun(Gun $gun): void
     {
         $this->guns[] = $gun;
     }
 
-    public function printGuns()
+    public function buyGun(string $name, string $paymentMethod, Customer $customer): void
+    {
+        if (($gun = $this->searchByName($name)) !== null && $this->isValidPaymentMethod($paymentMethod))
+        {
+            $getPayMethod = "get" . $paymentMethod;
+            $customer->$getPayMethod()->withdraw($gun->getPrice());
+            array_splice($this->guns, array_search($gun, $this->guns), 1);
+        }
+    }
+
+    private function searchByName(string $name): ?Gun
+    {
+        /** @var Gun $gun */
+        foreach ($this->guns as $gun)
+        {
+            if ($gun->getName() === $name)
+            {
+                return $gun;
+            }
+        }
+        return null;
+    }
+
+    private function isValidPaymentMethod(string $paymentMethod): bool
+    {
+        return $paymentMethod === "Wallet" || $paymentMethod === "PayPal" || $paymentMethod === "Bank";
+    }
+
+    public function printGuns(): void
     {
         foreach ($this->guns as $gun)
         {
