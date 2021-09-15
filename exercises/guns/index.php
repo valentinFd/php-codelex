b15de6adb5392f6e25683e7fc147b8d69053fe65
Valik <valikfad@gmail.com>
2021-09-15 15:43:15 +0300

    update index.php

--- exercises/guns/index.php
+++ exercises/guns/index.php
@@ -13,20 +13,34 @@ require_once("Customer.php");
 
 $shop = new GunShop();
 
-$shop->addGun(new Shotgun("M58B", 2000));
-$shop->addGun(new Shotgun("CM350M", 2000));
-$shop->addGun(new SniperRifle("SW-04J", 2000));
+$shop->addGun(new Shotgun("M58B", 1000));
+$shop->addGun(new Shotgun("CM350M", 3000));
+$shop->addGun(new SniperRifle("SW-04J", 6000));
 $shop->printGuns();
 
 $customer = new Customer(10000, 10000, 10000);
 $customer->printMoney();
 
+$name = readline("Enter gun name: ");
 $gun1 = $shop->searchByName("M58B");
+$paymentMethod = "get" . readline("Enter payment method (Wallet/PayPal/Bank): ");
+$shop->buyGun($gun1, $customer->$paymentMethod());
+echo PHP_EOL;
+$shop->printGuns();
+$customer->printMoney();
+
+$name = readline("Enter gun name: ");
 $gun2 = $shop->searchByName("CM350M");
+$paymentMethod = "get" . readline("Enter payment method (Wallet/PayPal/Bank): ");
+$shop->buyGun($gun2, $customer->$paymentMethod());
+echo PHP_EOL;
+$shop->printGuns();
+$customer->printMoney();
+
+$name = readline("Enter gun name: ");
 $gun3 = $shop->searchByName("SW-04J");
-
-$shop->buyGun($gun1, $customer->getWallet());
-$shop->buyGun($gun2, $customer->getPayPal());
-$shop->buyGun($gun3, $customer->getBank());
-
+$paymentMethod = "get" . readline("Enter payment method (Wallet/PayPal/Bank): ");
+$shop->buyGun($gun3, $customer->$paymentMethod());
+echo PHP_EOL;
+$shop->printGuns();
 $customer->printMoney();
