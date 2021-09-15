f507c75ea6e8cc7a63842a39b9eef2705c020409
Valik <valikfad@gmail.com>
2021-09-15 14:32:54 +0300

    add Customer.php

---
+++ exercises/guns/Customer.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types = 1);
+
+class Customer
+{
+    private Wallet $wallet;
+
+    public function getWallet(): Wallet
+    {
+        return $this->wallet;
+    }
+
+    private PayPal $payPal;
+
+    public function getPayPal(): PayPal
+    {
+        return $this->payPal;
+    }
+
+    private Bank $bank;
+
+    public function getBank(): Bank
+    {
+        return $this->bank;
+    }
+
+    public function __construct(int $walletMoney, int $paypalMoney, string $email, string $password, int $bankMoney)
+    {
+        $this->wallet = new Wallet($walletMoney);
+        $this->payPal = new PayPal();
+        $this->payPal->createAccount($paypalMoney, $email, $password);
+        $this->bank = new Bank($bankMoney);
+    }
+
+    public function printMoney(): string
+    {
+        $walletDollars = round(($this->wallet->getMoney() / 100), 2);
+        $payPalDollars = round(($this->payPal->getMoney() / 100), 2);
+        $bankDollars = round(($this->bank->getMoney() / 100), 2);
+        return "Wallet: $$walletDollars | PayPal: $$payPalDollars | Bank: $$bankDollars";
+    }
+}
