2c662252c43846f780e75a498b9d6db90ff6bfa6
Valik <valikfad@gmail.com>
2021-09-17 16:30:12 +0300

    update Cage.php

--- exercises/zoo/Enclosures/Cage.php
+++ exercises/zoo/Enclosures/Cage.php
@@ -1,9 +1,21 @@
 <?php
 
-class Cage extends Enclosure
+class Cage
 {
+    private ?ICagedAnimal $animal;
+
+    public function getAnimal(): ?ICagedAnimal
+    {
+        return $this->animal;
+    }
+
+    public function __construct()
+    {
+        $this->animal = null;
+    }
+
     public function addAnimal(ICagedAnimal $animal): void
     {
-        $this->animals[] = $animal;
+        $this->animal = $animal;
     }
 }
