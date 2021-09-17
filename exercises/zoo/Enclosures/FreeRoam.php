b355b0a4ae34be4d7939d808918b45531b7888fb
Valik <valikfad@gmail.com>
2021-09-17 16:30:24 +0300

    update FreeRoam.php

--- exercises/zoo/Enclosures/FreeRoam.php
+++ exercises/zoo/Enclosures/FreeRoam.php
@@ -1,9 +1,21 @@
 <?php
 
-class FreeRoam extends Enclosure
+class FreeRoam
 {
+    private ?IFreeRoamAnimal $animal;
+
+    public function getAnimal(): ?IFreeRoamAnimal
+    {
+        return $this->animal;
+    }
+
+    public function __construct()
+    {
+        $this->animal = null;
+    }
+
     public function addAnimal(IFreeRoamAnimal $animal): void
     {
-        $this->animals[] = $animal;
+        $this->animal = $animal;
     }
 }
